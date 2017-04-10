45a68204381f49b69915e31b10f50b01c2ce392d
Joey Fang <josudoey@gmail.com>
2017-04-10 10:20:02 +0800

    [modiy] support option for change basedir

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-var path = require('path');
-var resolve = require('resolve');
-var pug = require('pug');
+var path = require('path')
+var resolve = require('resolve')
+var pug = require('pug')
 
 var ___dirname = function () {
-  var stack = new Error().stack;
-  var lines = stack.split('\n');
-  var line = lines[3];
-  var m = line.match(/at (?:(.+)\s+\()?(?:(.+?):(\d+):(\d+)|([^)]+))\)?/);
-  return path.dirname(m[2]);
+  var stack = new Error().stack
+  var lines = stack.split('\n')
+  var line = lines[3]
+  var m = line.match(/at (?:(.+)\s+\()?(?:(.+?):(\d+):(\d+)|([^)]+))\)?/)
+  return path.dirname(m[2])
 }
 
-module.exports = function () {
-  var cache = {};
+module.exports = function (opts) {
+  opts = opts || {}
+  var cache = {}
   var resOpts = {
-    basedir: ___dirname(),
+    basedir: opts.basedir || ___dirname(),
     extensions: ['.pug']
   }
   return function (p, data) {
     if (p in cache) {
-      return cache[p](data);
+      return cache[p](data)
     }
 
-    var fp = resolve.sync(p, resOpts);
-    var fn = cache[p] = pug.compileFile(fp);
-    return fn(data);
+    var fp = resolve.sync(p, resOpts)
+    var fn = cache[p] = pug.compileFile(fp)
+    return fn(data)
   }
 }
-
